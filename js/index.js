4b1b6a71426ecd7e7174d3f8d213ee22b23e1488
tuistmessiah <pedrocaetano90@gmail.com>
2019-07-20 12:23:47 -0300

    Improved Projects, Folder organize

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
 // ---- Sliding Tabs
 $('.slide a').click(function () {
-    $('.slide.active').removeClass('active');
-    $(this).closest('.slide').addClass('active');
-    return false;
+  $('.slide.active').removeClass('active');
+  $(this).closest('.slide').addClass('active');
+  return false;
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
   });
+}
