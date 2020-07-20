f49ed9470591023720021aea748034ad07342731
Pedro Caetano <pedrocaetano90@gmail.com>
2020-07-20 14:24:42 +0200

    Content update 2020 July
    Added glowing button in Web Dev Button
    
    Changed padding in layout
    
    Added Nether API image and text in project

--- js/index.js
+++ js/index.js
@@ -3,13 +3,18 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+  acc[i].addEventListener("click", onClick);
 }
+
+function onClick() {
+  this.classList.toggle("active");
+  var panel = this.nextElementSibling;
+  if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+  } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+  }
+}
+
+// Defined default opened slide
+// onClick.call(acc[0]);
